b4bd9eae9c57aaece651f80633528be33236d889
Alex <alex@recordsonribs.com>
2015-11-29 18:07:36 +0000

    Conform Webpack config to style guide, add in asset compilation

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,59 @@
+/* eslint no-var: 0 */
 /* global __dirname */
-
 var path = require('path');
 
 var webpack = require('webpack');
 var CopyWebpackPlugin = require('copy-webpack-plugin');
 
-var dir_js = path.resolve(__dirname, 'js');
-var dir_html = path.resolve(__dirname, 'html');
-var dir_build = path.resolve(__dirname, 'build');
+var autoprefixer = require('autoprefixer');
+
+var dirJS = path.resolve(__dirname, 'js');
+var dirHTML = path.resolve(__dirname, 'html');
+var dirBuild = path.resolve(__dirname, 'build');
 
 module.exports = {
-    entry: path.resolve(dir_js, 'main.js'),
-    output: {
-        path: dir_build,
-        filename: 'bundle.js'
-    },
-    devServer: {
-        contentBase: dir_build,
-    },
-    module: {
-        loaders: [
-            {
-                loader: 'react-hot',
-                test: dir_js,
-            },
-            {
-                loader: 'babel-loader',
-                test: dir_js,
-                query: {
-                    presets: ['es2015', 'react'],
-                },
-            }
-        ]
-    },
-    plugins: [
-        // Simply copies the files over
-        new CopyWebpackPlugin([
-            { from: dir_html } // to: output.path
-        ]),
-        // Avoid publishing files when compilation fails
-        new webpack.NoErrorsPlugin()
+  entry: path.resolve(dirJS, 'main.js'),
+  output: {
+    path: dirBuild,
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: dirBuild,
+  },
+  module: {
+    loaders: [
+      {
+        loader: 'react-hot',
+        test: dirJS,
+      },
+      {
+        loader: 'babel-loader',
+        test: dirJS,
+        query: {
+          presets: ['es2015', 'react'],
+        },
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader!postcss-loader',
+      },
     ],
-    stats: {
-        // Nice colored output
-        colors: true
-    },
-    // Create Sourcemaps for the bundle
-    devtool: 'source-map',
+  },
+  postcss: function postCSS() {
+    return [autoprefixer];
+  },
+  plugins: [
+    // Simply copies the files over
+    new CopyWebpackPlugin([
+      { from: dirHTML },
+    ]),
+    // Avoid publishing files when compilation fails
+    new webpack.NoErrorsPlugin(),
+  ],
+  stats: {
+    // Nice colored output
+    colors: true,
+  },
+  // Create Sourcemaps for the bundle
+  devtool: 'source-map',
 };
