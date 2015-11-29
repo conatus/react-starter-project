4b1ab22227f46f64217803d4946aa030fdda01c5
Alex <alex@recordsonribs.com>
2015-11-29 20:22:25 +0000

    Introduce React back into the mix.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
 import '../css/main.css';
+
+import React from 'react';
+import { render } from 'react-dom';
+
+class Main extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (<div>React works</div>);
+  }
+}
+
+render(<Main/>, document.getElementById('main'));
